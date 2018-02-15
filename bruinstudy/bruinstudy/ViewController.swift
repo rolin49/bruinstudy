056ff6d013fedf54232de14e349156831702f825
“rolin49” <royson.lin+github@gmail.com>
2018-02-14 20:12:18 -0800

    first commit omg hopefully this works

---
+++ bruinstudy/bruinstudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bruinstudy
+//
+//  Created by Royson Lin on 2/14/18.
+//  Copyright © 2018 Awk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
