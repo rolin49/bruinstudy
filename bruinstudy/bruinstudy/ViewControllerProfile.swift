6fae1e63d07083979fbfef7ba6235934af36dc7c
“rolin49” <royson.lin+github@gmail.com>
2018-02-21 19:46:06 -0800

    add viewcontrollers

---
+++ bruinstudy/bruinstudy/ViewControllerProfile.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerProfile.swift
+//  bruinstudy
+//
+//  Created by Royson Lin on 2/21/18.
+//  Copyright © 2018 Awk. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerProfile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
