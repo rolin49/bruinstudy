7d430893a17f7340b767a8fa11d256c66bf9e527
Megan <defaultuser@s-169-232-108-200.resnet.ucla.edu>
2018-03-05 21:00:13 -0800

    passwords

---
+++ bruinstudy/bruinstudy/ViewControllerMessage.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerMessage.swift
+//  bruinstudy
+//
+//  Created by Megan on 3/5/18.
+//  Copyright © 2018 Awk. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerMessage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
